3e21b07feb9308c2051d5a17e70fcd264a2ac562
DouglasWu <dgrey1116@gmail.com>
2017-06-01 03:01:57 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { Row, Col, Input, Button } from 'reactstrap'; //'react-materialize';
+import Canvas from './canvas';
+import './App.css';
+
+const examples = {
+  river: ['淡水河', '頭前溪', '中港溪', '後龍溪', '大安溪',
+          '大甲溪', '烏溪', '濁水溪', '北港溪', '朴子溪',
+          '八掌溪', '急水溪', '曾文溪', '鹽水溪', '二仁溪',
+          '阿公店溪', '高屏溪', '東港溪', '林邊溪', '四重溪',
+          '蘭陽溪', '和平溪', '花蓮溪', '秀姑巒溪', '卑南溪'],
+  dumai: ['後頂', '強間', '腦戶', '風府', '啞門',
+          '大椎', '陶道', '身柱', '神道', '靈台',
+          '至陽', '筋縮', '中樞', '脊中', '懸樞',
+          '命門', '陽關', '腰俞', '長強', '',
+          '', '', '', '', ''],
+  pi: ['14159', '26535', '89793', '23846', '26433',
+       '83279', '50288', '41971', '69399', '37510',
+       '58209', '74944', '59230', '78164', '06286',
+       '20899', '86280', '34825', '34211', '70679',
+       '82148', '08651', '32823', '06647', '09384']
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: Array(25).fill('')
+    };
+  }
+
+  example = (id) => {
+    this.setState({items: examples[id]});
+  }
+  clearAll = () => {
+    this.setState({items: Array(25).fill('')});
+  }
+  download = () => {
+    let link = document.createElement('a');
+    link.href = this.refs.canvas.refs.canvas.toDataURL();
+    link.download = `nbu.png`;
+    link.click();
+  }
+
+  changeItems = (idx, value) => {
+    var arr = this.state.items.slice();
+    arr.splice(idx, 1, value);
+    this.setState({items: arr});
+  }
+  renderInputs() {
+    const arr = [1, 2, 3, 4, 5];
+    return arr.map((num, i) => (
+      <Col md={{size: 2, offset: num==1?1:0}} xs={{size: 10, offset: 1}} key={i}>
+        {
+          arr.map((e, j) => {
+            const idx = (num-1)*5 + e - 1;
+            return <Input
+              key={j}
+              className="item-input"
+              placeholder={idx+1}
+              value={this.state.items[idx]}
+              onChange={e => this.changeItems(idx, e.target.value)}
+            />
+          })
+        }
+      </Col>
+    ));
+  }
+  render() {
+    return (
+      <div>
+        <Row>
+          { this.renderInputs() }
+
+        </Row>
+        <Row style={{marginTop: '5px'}}>
+          <Col md={{offset: 1}} xs={{size: 10, offset: 1}}>
+            <Button onClick={()=>this.example('river')} color="primary">台灣河流</Button>{' '}
+            <Button onClick={()=>this.example('dumai')} color="primary">督脈</Button>{' '}
+            <Button onClick={()=>this.example('pi')} color="primary">圓周率</Button>{' '}
+            <Button onClick={this.clearAll}>清空</Button>{' '}
+            <Button onClick={this.download} color="success">下載電震圖</Button>
+          </Col>
+        </Row>
+        <Row style={{marginTop: '15px'}}>
+          <div className="canvas-wrapper">
+            <Canvas items={this.state.items} ref="canvas"/>
+          </div>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/canvas.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.updateCanvas();
+  }
+  componentDidUpdate(prevProps, prevState) {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const c = this.refs.canvas;
+    const w = c.width-40;
+    const h = c.height-40;
+    const ctx = c.getContext("2d");
+    const R = h/2;
+    const r = h/6;
+    const centerX = w/2+20;
+    const centerY = h/2+20;
+    const yOffset = h/35;
+    const fontSize = h/26;
+    const textColor = "#000";
+    const heightLightColor = "#FFD480";
+    const indexColor = "#F95959";
+    const strokeColor = "#ADA6AB";
+    ctx.clearRect(0, 0, w, h);
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = `${fontSize}px Helvetica`;
+
+    var texts = this.props.items;
+
+    // five inner cirlces' centers
+    var centerPos = [{x: centerX, y:centerY}];
+    for(var i=Math.PI/4; i<Math.PI*2; i+=Math.PI/2) {
+      centerPos.push({x: centerX + 2*r*Math.cos(i),
+      y: centerY+2*r*Math.sin(i)});
+    }
+    const orders = [3, 2, 0, 4, 1];
+    var textPos = [];
+    orders.forEach(num => {
+      const {x, y} = centerPos[num];
+      textPos.push({ x, y: y-r*2/3 });
+      textPos.push({ x, y: y-r*1/3 });
+      textPos.push({ x, y: y });
+      textPos.push({ x, y: y+r*1/3 });
+      textPos.push({ x, y: y+r*2/3 });
+    });
+
+    // draw big outer circle
+    ctx.beginPath();
+    ctx.lineWidth=8;
+    ctx.strokeStyle=strokeColor;
+    ctx.arc(centerX,centerY,R,0,2*Math.PI);
+    ctx.stroke();
+
+    // draw five inner circles
+    centerPos.forEach((e, idx) => {
+      ctx.beginPath();
+      ctx.arc(e.x, e.y, r, 0, 2*Math.PI);
+      ctx.lineWidth=5;
+      ctx.stroke();
+      ctx.fillStyle = heightLightColor;
+      ctx.fillRect(e.x-r+yOffset/2, e.y-yOffset , 2*r-yOffset, yOffset*2);
+    });
+
+    // draw texts
+    textPos.forEach((e, idx) => {
+      ctx.fillStyle=textColor;
+      if(texts[idx])
+        ctx.fillText(`${texts[idx]}`, e.x, e.y);
+      // draw key index
+      if(idx%5==2) {
+        ctx.fillStyle = indexColor;
+        ctx.fillText(`${idx+1}.`, e.x-r+yOffset*1.8, e.y);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <canvas ref="canvas" width="800" height="800">
+        Your browser does not support the HTML5 canvas tag.
+      </canvas>
+    );
+  }
+}
+
+export default Canvas;
